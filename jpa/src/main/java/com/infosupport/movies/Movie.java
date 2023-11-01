1dec1af33d5a75e01c88312ade80a893ddcf20a9
Hanno Embregts <hanno.embregts@infosupport.com>
2023-11-01 15:57:58 +0100

    Extend JPA demo

--- jpa/src/main/java/com/infosupport/movies/Movie.java
+++ jpa/src/main/java/com/infosupport/movies/Movie.java
@@ -2,16 +2,21 @@ package com.infosupport.movies;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Movie {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long movieId;
     private String name;
 
     public Movie(String name) {
         this.name = name;
+    }
+
+    public Movie() {
+
     }
 }
--- jpa/src/test/java/com/infosupport/movies/MovieTest.java
+++ jpa/src/test/java/com/infosupport/movies/MovieTest.java
@@ -3,16 +3,37 @@ package com.infosupport.movies;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
+    private static EntityManager em;
+
+    @BeforeAll
+    static void setup() {
+        em = Persistence.createEntityManagerFactory("movies").createEntityManager();
+    }
+
     @Test
     void persistMovie() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("movies");
-        EntityManager em = emf.createEntityManager();
+        var transaction = em.getTransaction();
+        transaction.begin();
+
         Movie goodfellas = new Movie("Goodfellas");
+        Movie theGodfather = new Movie("The Godfather");
+
         em.persist(goodfellas);
+        em.persist(theGodfather);
+
+        transaction.commit();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        em.close();
     }
 }
